9603e124fa19aa674fca329320312de1b804e4b2
Cory Harkins <charkins@nossi.edu>
2018-03-09 15:22:00 -0600

    added read/write sync

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 /******************************
 				#4 Global Object
 ******************************/
-console.log('hello world'); //prints to terminal/console
+// console.log('hello world'); //prints to terminal/console
 
-setTimeout(function(){ console.log('hello again')}, 3000); // logs 'hello again' after 3 seconds
-let time = 0;
-let timer = setInterval(function(){
-	console.log('and again')
-	time++;
-	if (time > 3) {
-		clearInterval(timer); //clears the loop after time has reached 3
-	}
-}, 1000); //logs 'and again' 4 times
-console.log(__dirname); //returns directory
-console.log(__filename); //returns file
+// setTimeout(function(){ console.log('hello again')}, 3000); // logs 'hello again' after 3 seconds
+// let time = 0;
+// let timer = setInterval(function(){
+// 	console.log('and again')
+// 	time++;
+// 	if (time > 3) {
+// 		clearInterval(timer); //clears the loop after time has reached 3
+// 	}
+// }, 1000); //logs 'and again' 4 times
+// console.log(__dirname); //returns directory
+// console.log(__filename); //returns file
 
 /*****************************************
 				#5 Function Expressions
 ******************************************/
 
 //normal function statement 
-function name(arg1) {
-	console.log(arg1);
-}
+// function name(arg1) {
+// 	console.log(arg1);
+// }
 
-name('hi');
+// name('hi');
 
-//function expression
+// //function expression
 
-let sayBye = function(){
-	console.log('bye');
-};
+// let sayBye = function(){
+// 	console.log('bye');
+// };
 
-sayBye();
+// sayBye();
 
-//function expression pattern is common in node
+// //function expression pattern is common in node
 
-function callFunction(fun) {
-	fun();
-} //this is a callback
+// function callFunction(fun) {
+// 	fun();
+// } //this is a callback
 
 
-callFunction(sayBye);
+// callFunction(sayBye);
 
 
 /*****************************************
@@ -50,31 +50,69 @@ callFunction(sayBye);
 
 // Modules are a logical split of your program
 
-let counter = require('./count');
+// let counter = require('./count');
 
-console.log(counter(['shaun', 'crystal', 'ryu']));
+// console.log(counter(['shaun', 'crystal', 'ryu']));
 
 
 /*****************************************
 				#7 Module Patterns
 ******************************************/
-let stuff = require('./stuff');
+// let stuff = require('./stuff');
 
-console.log(stuff.evaluate(3,5,'multiply'));
-console.log(stuff.compare(2,2));
+// console.log(stuff.evaluate(3,5,'multiply'));
+// console.log(stuff.compare(2,2));
+
+/*****************************************
+				#8 The Event Emiiter
+******************************************/
+
+// const stuff = require('./stuff');
+// const events = require('events');
+// const util = require('util');
+// Just like vanilla ES, we can listen for events
+//element.on('click', function(){ do something });
+
+// const myEmitter = new events.EventEmitter();
+
+// myEmitter.on('someEvent', function(msg){
+// 	console.log(msg);
+// });
+
+// myEmitter.emit('someEvent', 'Hello World');
+
+// let Person = function(name) {
+// 	this.name = name;
+// };
+
+// util.inherits(Person, events.EventEmitter);
+
+// const james = new Person('james');
+// const mary = new Person('mary');
+// const joe= new Person('joe');
+// let people = [james,mary,joe];
+
+// people.forEach(function(person){
+// 	person.on('speak', function(msg){
+// 		console.log(person.name + ' said ' + msg);
+// 	});
+// });
+
+// james.emit('speak','hey dudes');
+// joe.emit('speak', 'I want curry');
 
 
+/*****************************************
+				#9 Reading and Writing files
+******************************************/
 
+const fs = require('fs');
+const path = require('path');
 
-
-
-
-
-
-
-
-
-
+// Blocking code
+const readMe = fs.readFileSync('./readMe.txt', 'utf-8');
+// console.log(readMe);
+fs.writeFileSync('./writeMe.txt', readMe);
 
 
 
--- stuff.js
+++ stuff.js
@@ -14,5 +14,10 @@
 let evaluate = (num1,num2,operator)=>operator.toUpperCase()==="ADD"?num1+num2:operator.toUpperCase()==="SUBTRACT"?num1-num2:operator.toUpperCase()==="MULTIPLY"?num1*num2:operator.toUpperCase()==="DIVIDE"?num1/num2:null;
 let compare = (num1,num2)=>num1===num2?`The number ${num1} is equal to ${num2}`:`${num1} is not equal to ${num2}`;
 
-module.exports.evaluate = evaluate;
-module.exports.compare = compare;
+// module.exports.evaluate = evaluate;
+// module.exports.compare = compare;
+
+module.exports = {
+	evaluate: evaluate,
+	compare: compare
+}
